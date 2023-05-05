71715b7da5dfd8240f27dbf9e11986538d742701
Julian Masso <jamassol@uqvirtual.edu.co>
2023-05-05 13:40:14 -0500

    ensayo despliegue 1 solo modulo 19

--- src/main/java/co/edu/uniquindio/odontologia/NegocioApplication.java
+++ src/main/java/co/edu/uniquindio/odontologia/NegocioApplication.java
@@ -2,9 +2,18 @@ package co.edu.uniquindio.odontologia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NegocioApplication {
+    @GetMapping("/mensaje")
+
+    public String mensaje(){
+        return "hola";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(NegocioApplication.class, args);
     }
