2bae5e5618f9365b9e21bb74ce20a3a52fd5f5c5
Juan Esteban Henao <juanes13916@gmail.com>
2023-05-21 10:51:36 -0500

    Modificacion de index 3

--- src/main/java/co/edu/uniquindio/odontologia/bean/InicioBean.java
+++ src/main/java/co/edu/uniquindio/odontologia/bean/InicioBean.java
@@ -32,6 +32,7 @@ public class InicioBean implements Serializable {
             imagenes.add("https://www.teeth22.com/wp-content/uploads/2020/03/absceso-dental-1024x511.png");
             imagenes.add("https://staticnew-prod.topdoctors.cl/files/Image/large/226302beaa60ee5802b6efaf2afecb18.jpg");
             imagenes.add("https://mafercamargo.com/wp-content/uploads/2021/03/servicio_Odontologia_laser_Bogota_Mafer_Camargo-1024x538.jpg");
+            imagenes.add("https://img.genial.ly/60ea5207a0a9040fc032feff/fd51acc0-056b-4d57-8cc1-02231e623ea6.png");
         }catch (Exception e){
             throw  new RuntimeException(e);
         }
